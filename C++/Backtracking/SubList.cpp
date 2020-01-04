42408411c6dcffd4e980b61d3606ab235f126be9
Emrullah Tasdemir <42010240+emtsdmr@users.noreply.github.com>
2020-01-04 21:03:55 +0300

    Update SubList.cpp

--- C++/Backtracking/SubList.cpp
+++ C++/Backtracking/SubList.cpp
@@ -22,18 +22,18 @@ void subLists(vector <string>& available, vector <string>& chosen)
 		print(chosen);
 
 	else
-		{
-			//there are two choices to explore
-			string c = available[0];
-			chosen.push_back(c);
-			available.erase(available.begin()); 
-			subLists(available, chosen); // choose and explore (with)
+	{
+		//there are two choices to explore
+		string c = available[0];
+		chosen.push_back(c);
+		available.erase(available.begin()); 
+		subLists(available, chosen); // choose and explore (with)
 
-			chosen.pop_back();
-			subLists(available, chosen); // choose and explore (without)
-			
-			available.insert(available.begin(),c); // un-choose in order to continue to search
-		}
+		chosen.pop_back();
+		subLists(available, chosen); // choose and explore (without)
+
+		available.insert(available.begin(),c); // un-choose in order to continue to search
+	}
 }
 
 void print(vector<string>& ar)
